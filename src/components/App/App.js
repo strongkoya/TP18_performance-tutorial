ef9aa56945fcd9219e36713dd28c207339395694
Toumi Mohamed Koussay <toumikoussay@hotmail.fr>
2022-12-18 02:16:40 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React, { useCallback, useReducer, useState } from 'react';
+import './App.css';
+
+import CharacterMap from '../CharacterMap/CharacterMap';
+
+function App() {
+  const [text, setText] = useState('');
+  const [showExplanation, toggleExplanation] = useReducer(state => !state, false)
+  const transformer = useCallback(item => item.toLowerCase(), []);
+
+  return(
+    <div className="wrapper">
+      <label htmlFor="text">
+        <p>Your Text</p>
+        <textarea
+          id="text"
+          name="text"
+          rows="10"
+          cols="100"
+          onChange={event => setText(event.target.value)}
+        >
+        </textarea>
+      </label>
+      <div>
+        <button onClick={toggleExplanation}>Show Explanation</button>
+      </div>
+      <CharacterMap showExplanation={showExplanation} text={text} transformer={transformer} />
+    </div>
+  )
+}
+
+export default App;
